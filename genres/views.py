45709692e8fb63a8653db887d0ee05ef389dfa5d
Fábio Silva <linkineo2002@hotmail.com>
2025-08-29 17:47:33 -0300

    refatorar: adicionar autenticação JWT e permissões globais para as views de atores, gêneros, filmes e avaliações

--- genres/views.py
+++ genres/views.py
@@ -1,11 +1,15 @@
-from genres.models import Genre
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from app.permissions import GlobalDefaultPermission
+from genres.models import Genre
 from genres.serializers import GenreSerializer
 
 class GenreListCreateView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
 class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
