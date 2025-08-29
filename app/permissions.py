baf96b085cfa62c2a3d07053ff32ac612ba33d88
Fábio Silva <linkineo2002@hotmail.com>
2025-08-29 17:48:37 -0300

    refatorar: corrigir erro de digitação no método has_permission

--- app/permissions.py
+++ app/permissions.py
@@ -9,7 +9,7 @@ class GlobalDefaultPermission(permissions.BasePermission):
         )
         if not model_permission_codename:
             return False
-        
+    
         return request.user.has_perm(model_permission_codename)
 
     def __get_model_permission_codename(self, method, view):
